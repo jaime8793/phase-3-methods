cf6716336f233aba53c12bc0d39be021e8e337e7
James Owiti Maurice <james.owiti@student.moringaschool.com>
2022-10-19 02:10:56 +0300

    Completed Assignment

--- methods.rb
+++ methods.rb
@@ -1,1 +1,38 @@
 # Your code here!
+def greet_programmer
+  puts "Hello, programmer!"
+
+end
+  greet_programmer
+
+def greet (a_name)
+  puts "Hello, #{a_name}!"
+
+end
+
+  greet ("name")
+
+def greet_with_default(name = "programmer")
+
+  puts "Hello, #{name}!"
+
+end
+
+def add (num1, num2)
+
+  return num1 + num2
+
+end
+
+  add(1, 2)
+
+  def halve nos
+    if nos.class !=Integer
+      return nil
+    end
+
+
+    nos/2
+
+  end
+  halve 6
